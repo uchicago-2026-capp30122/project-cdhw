83eca7e4526dac09c25e826bdb85fe7fbae882c2
Hannah Barton <hbarton1@uchicago.edu>
2026-03-07 17:54:07 -0600

    new ca census file

--- src/dash_app/config.py
+++ src/dash_app/config.py
@@ -4,6 +4,8 @@ TRACT_GEOJSON = "data/processed/tracts_chicago.geojson"
 CA_CSV = "data/processed/community_area_census.csv"
 CA_GEOJSON = "data/processed/community_areas.geojson"
 
+RIDESHARE_COMMUNITY_JSON = "data/processed/rideshare_community_areas.json"
+
 # Variables you want available in dropdown (works for both geographies)
 DROPDOWN_VARS = [
     "transportation_need_index_0_100",

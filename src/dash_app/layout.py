2bc6ebc03272ff108278094e2edc98743ad1a5c8
wendyhtwang <wendyhtwang@uchicago.edu>
2026-03-08 13:40:48 -0500

    added toggles for overlaying CTA points & rideshare points

--- src/dash_app/layout.py
+++ src/dash_app/layout.py
@@ -10,6 +10,7 @@ from src.dash_app.generate_rideshare_html import generate_html
 def make_layout(map_vars):
     return html.Div(
         style={"maxWidth": "1100px", "margin": "0 auto", "padding": "16px"},
+        
         children=[
             html.H2("Chicago-area Transportation Need Map (MVP)"),
 
@@ -24,6 +25,17 @@ def make_layout(map_vars):
                 inline=True,
             ),
 
+            # toggle for overlay (CTA ridership @station lat/lon & rideshare ridership at community area centroid)
+            dcc.Checklist(
+                id="overlay-toggle",
+                options=[
+                    {"label": "CTA stations", "value": "cta"},
+                    {"label": "Rideshare totals", "value": "rideshare"},
+                ],
+                value=[],
+                inline=True,
+            ),
+            
             html.Br(),
             html.Label("Variable"),
             dcc.Dropdown(
@@ -35,6 +47,7 @@ def make_layout(map_vars):
 
             dcc.Graph(id="choropleth", style={"height": "75vh"}),
 
+            # block for network analysis graph
             html.Div([
                 html.H1(children = 'Rideshares to and from Chicago community areas (2025)'),
 

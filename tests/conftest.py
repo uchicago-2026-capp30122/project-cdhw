8df58dc0f100d82dc097a8e14828a68a3b4f1347
dhoughton <dhoughton@uchicago.edu>
2026-03-08 22:58:28 -0500

    Edit api client

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+# Add the project root directory to the Python path
+# so that `src` module can be imported by tests
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
